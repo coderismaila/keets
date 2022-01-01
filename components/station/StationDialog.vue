5defe6d21a3abd53eb849860378ffc9c4827f5cc
Ismaila Hassan <code.ismailah28@gmail.com>
2022-01-01 12:56:26 +0100

    refactor: extract create-update station dialog

--- components/station/StationDialog.vue
+++ components/station/StationDialog.vue
@@ -1,175 +1,280 @@
 <template>
-  <v-dialog v-model="dialog2" max-width="400px">
+  <v-dialog v-model="dialog" max-width="400px">
     <template #activator="{ on, attrs }">
-      <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+      <v-btn tile color="primary" dark v-bind="attrs" v-on="on">
         New Station
         <v-icon right dark> mdi-plus </v-icon>
       </v-btn>
     </template>
-    <v-card>
+    <v-card class="py-4">
       <v-card-title>
         <span class="text-h5">{{ formTitle }}</span>
       </v-card-title>
       <v-card-text>
         <v-container>
-          <v-alert v-if="station_error" type="error" dismissible>{{
+          <v-alert v-if="station_error" dense type="error" dismissible>{{
             station_error_message
           }}</v-alert>
           <v-row>
-            <v-col cols="12" class="pa-0">
+            <v-col cols="12" class="pb-0">
               <v-text-field
-                v-model="editedItem2.name"
-                :rules="[required]"
+                v-model="editedItem.name"
                 outlined
                 dense
                 placeholder="Station Name"
               ></v-text-field>
             </v-col>
-            <v-col cols="12" class="pa-0">
-              <v-select
-                v-model="editedItem2.areaOfficeName"
+            <v-col cols="12" class="py-0">
+              <v-combobox
+                v-model="editedItem.areaOfficeName"
                 :items="areaOfficeNames"
-                :rules="[required]"
                 cache-items
-                :error="error"
-                :error-messages="error_message"
+                :error="area_office_error"
+                :error-messages="area_office_error_message"
                 placeholder="Area Office"
                 outlined
                 dense
                 :loading="loading"
-              ></v-select>
+              ></v-combobox>
             </v-col>
-            <v-col cols="12" class="pa-0">
-              <v-radio-group
-                v-model="editedItem2.stationType"
-                row
-                :rules="[required]"
-              >
+            <v-col cols="12" class="py-0">
+              <p class="my-0">Station Type</p>
+              <v-radio-group v-model="editedItem.stationType" row class="my-0">
                 <v-radio label="Transmission" value="TRANSMISSION"></v-radio>
                 <v-radio label="Distribution" value="DISTRIBUTION"></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
+          <v-divider />
+          <v-row class="pt-4">
+            <v-expansion-panels
+              v-show="editedIndex === -1"
+              v-model="panel"
+              flat
+              class="elevation-0"
+            >
+              <v-expansion-panel>
+                <v-expansion-panel-header
+                  >Add Power Transformer</v-expansion-panel-header
+                >
+                <v-expansion-panel-content>
+                  <v-row>
+                    <v-col
+                      v-for="(input, i) in editedItem.powerTransformer"
+                      :key="i"
+                      cols="12"
+                    >
+                      <v-text-field
+                        v-model="input.name"
+                        outlined
+                        dense
+                        placeholder="Transformer Name"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model.number="input.capacityKVA"
+                        type="number"
+                        outlined
+                        dense
+                        placeholder="CapacityKVA"
+                      ></v-text-field>
+                      <v-tooltip bottom>
+                        <template #activator="{ on, attrs }">
+                          <v-btn
+                            v-show="
+                              i ||
+                              (!i && editedItem.powerTransformer.length > 1)
+                            "
+                            icon
+                            dark
+                            color="red"
+                            v-bind="attrs"
+                            @click="remove(i, editedItem.powerTransformer)"
+                            v-on="on"
+                          >
+                            <v-icon>mdi-delete-outline</v-icon>
+                          </v-btn>
+                        </template>
+                        <span>remove</span>
+                      </v-tooltip>
+                      <v-tooltip bottom>
+                        <template #activator="{ on, attrs }">
+                          <v-btn
+                            v-show="i == editedItem.powerTransformer.length - 1"
+                            color="primary"
+                            small
+                            icon
+                            v-bind="attrs"
+                            @click="add(input, editedItem.powerTransformer)"
+                            v-on="on"
+                          >
+                            <v-icon>mdi-plus</v-icon>
+                          </v-btn>
+                        </template>
+                        <span>add</span>
+                      </v-tooltip>
+                    </v-col>
+
+                    <v-col cols="12">
+                      <div class="d-flex">
+                        <v-spacer />
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-row>
         </v-container>
       </v-card-text>
 
       <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text @click.native="close"> Cancel </v-btn>
-        <v-btn color="primary" text @click="save"> Save </v-btn>
+        <v-spacer />
+        <v-btn small tile @click.native="close"> Cancel </v-btn>
+        <v-btn small tile color="primary" :loading="loading" @click="save">
+          {{ buttonText }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 <script>
+import { Notify } from 'notiflix'
 import { mapActions, mapGetters, mapState } from 'vuex'
-import rules from '~/mixins/rules'
 
 export default {
   name: 'StationDialog',
-  mixins: [rules],
   props: {
-    dialog: {
+    item: {
+      type: Object,
       required: true,
+    },
+    dialogProp: {
       type: Boolean,
-    },
-    editedIndex: {
       required: true,
+    },
+    editedItemProp: {
+      type: Object,
+      required: true,
+    },
+    editedIndexProp: {
       type: Number,
-    },
-    editedItem: {
       required: true,
-      type: Object,
-      name: {
-        required: true,
-        type: String,
-      },
-      stationType: {
-        required: true,
-        type: String,
-      },
-      areaOfficeName: {
-        required: true,
-        type: String,
-      },
-    },
-    defaultItem: {
-      required: true,
-      type: Object,
     },
   },
   data() {
     return {
+      // dialog: false,
+
+      defaultItem: {
+        name: '',
+        stationType: '',
+        areaOfficeName: '',
+        powerTransformer: [
+          {
+            name: '',
+            capacityKVA: '',
+          },
+        ],
+      },
       loading: false,
+      panel: [0],
     }
   },
   computed: {
-    ...mapState('area-office', ['areaOffices', 'error', 'error_message']),
+    ...mapGetters('station', ['stationTableData']),
+    ...mapState('area-office', {
+      areaOffices: 'areaOffices',
+      area_office_error: 'error',
+      area_office_error_message: 'error_message',
+    }),
     ...mapState('station', {
       station_error: 'error',
       station_error_message: 'error_message',
     }),
     ...mapGetters('area-office', ['areaOfficeNames']),
-    dialog2: {
-      get() {
-        return this.dialog
-      },
-      set(dialog) {
-        this.$emit('input', dialog)
-      },
-    },
-    editedIndex2: {
-      get() {
-        return this.editedIndex
-      },
-      set(editedIndex) {
-        this.$emit('input2', editedIndex)
-      },
-    },
-    editedItem2: {
-      get() {
-        return this.editedItem
-      },
-      set(editedItem) {
-        this.$emit('input3', editedItem)
-      },
-    },
-    defaultItem2: {
-      get() {
-        return this.defaultItem
-      },
-      set(defaultItem) {
-        this.$emit('input4', defaultItem)
-      },
-    },
+
     formTitle() {
       return this.editedIndex === -1 ? 'New Station' : 'Edit Station'
     },
+    buttonText() {
+      return this.editedIndex === -1 ? 'Save' : 'Update'
+    },
+    dialog: {
+      get() {
+        return this.dialogProp
+      },
+      set(dialogProp) {
+        this.$emit('update:dialogProp', dialogProp)
+      },
+    },
+
+    editedItem: {
+      get() {
+        return this.editedItemProp
+      },
+      set(editedItemProp) {
+        this.$emit('update:editedItemProp', editedItemProp)
+      },
+    },
+    editedIndex: {
+      get() {
+        return this.editedIndexProp
+      },
+      set(editedIndexProp) {
+        this.$emit('update:editedIndexProp', editedIndexProp)
+      },
+    },
   },
-  async created() {
-    this.loading = true
-    await this.$store.dispatch('area-office/getAllAreaOffices')
-    this.loading = false
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
   },
 
   methods: {
-    ...mapActions('station', ['addStation', 'updateStation']),
+    ...mapActions('station', ['addStation', 'updateStation', 'deleteStation']),
 
-    close() {
-      this.dialog2 = false
-      this.$nextTick(() => {
-        this.editedItem2 = Object.assign({}, this.defaultItem2)
-        this.editedIndex2 = -1
-      })
-    },
-    save() {
-      if (this.editedIndex > -1) {
-        this.updateStation(this.editedItem)
-      } else {
-        this.addStation(this.editedItem)
+    add(object, fieldType) {
+      const newObject = {}
+
+      // eslint-disable-next-line no-unused-vars
+      for (const [key, value] of Object.entries(object)) {
+        newObject[key] = ''
       }
+      fieldType.push(newObject)
+    },
+
+    remove(index, fieldType) {
+      fieldType.splice(index, 1)
+    },
+
+    async save() {
+      this.loading = true
+      if (this.editedIndex > -1) {
+        await this.updateStation(this.editedItem)
+      } else {
+        await this.addStation(this.editedItem)
+      }
+      this.loading = false
       // TODO: revisit this
+      if (this.station_error) {
+        return
+      }
       this.close()
+      Notify.success(
+        `${this.editedItem.name} has been ${
+          this.editedIndex > -1 ? 'updated' : 'added'
+        }`
+      )
+    },
+    close() {
+      this.dialog = false
+      this.$store.commit('station/SET_ERROR')
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
   },
 }
--- pages/assets/station.vue
+++ pages/assets/station.vue
@@ -8,183 +8,19 @@
       :items-per-page="5"
       :loading="$fetchState.pending"
     >
-      <template #top>
+      <template #top="{ item }">
         <v-toolbar flat>
           <v-toolbar-title>Station</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="400px">
-            <template #activator="{ on, attrs }">
-              <v-btn
-                tile
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                New Station
-                <v-icon right dark> mdi-plus </v-icon>
-              </v-btn>
-            </template>
-            <v-card class="py-4">
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-alert
-                    v-if="station_error"
-                    dense
-                    type="error"
-                    dismissible
-                    >{{ station_error_message }}</v-alert
-                  >
-                  <v-row>
-                    <v-col cols="12" class="pb-0">
-                      <v-text-field
-                        v-model="editedItem.name"
-                        outlined
-                        dense
-                        placeholder="Station Name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" class="py-0">
-                      <v-combobox
-                        v-model="editedItem.areaOfficeName"
-                        :items="areaOfficeNames"
-                        cache-items
-                        :error="area_office_error"
-                        :error-messages="area_office_error_message"
-                        placeholder="Area Office"
-                        outlined
-                        dense
-                        :loading="loading"
-                      ></v-combobox>
-                    </v-col>
-                    <v-col cols="12" class="py-0">
-                      <p class="my-0">Station Type</p>
-                      <v-radio-group
-                        v-model="editedItem.stationType"
-                        row
-                        class="my-0"
-                      >
-                        <v-radio
-                          label="Transmission"
-                          value="TRANSMISSION"
-                        ></v-radio>
-                        <v-radio
-                          label="Distribution"
-                          value="DISTRIBUTION"
-                        ></v-radio>
-                      </v-radio-group>
-                    </v-col>
-                  </v-row>
-                  <v-divider />
-                  <v-row class="pt-4">
-                    <v-expansion-panels
-                      v-show="editedIndex === -1"
-                      v-model="panel"
-                      flat
-                      class="elevation-0"
-                    >
-                      <v-expansion-panel>
-                        <v-expansion-panel-header
-                          >Add Power Transformer</v-expansion-panel-header
-                        >
-                        <v-expansion-panel-content>
-                          <v-row>
-                            <v-col
-                              v-for="(input, i) in editedItem.powerTransformer"
-                              :key="i"
-                              cols="12"
-                            >
-                              <v-text-field
-                                v-model="input.name"
-                                outlined
-                                dense
-                                placeholder="Transformer Name"
-                              ></v-text-field>
-                              <v-text-field
-                                v-model.number="input.capacityKVA"
-                                type="number"
-                                outlined
-                                dense
-                                placeholder="CapacityKVA"
-                              ></v-text-field>
-                              <v-tooltip bottom>
-                                <template #activator="{ on, attrs }">
-                                  <v-btn
-                                    v-show="
-                                      i ||
-                                      (!i &&
-                                        editedItem.powerTransformer.length > 1)
-                                    "
-                                    icon
-                                    dark
-                                    color="red"
-                                    v-bind="attrs"
-                                    @click="
-                                      remove(i, editedItem.powerTransformer)
-                                    "
-                                    v-on="on"
-                                  >
-                                    <v-icon>mdi-delete-outline</v-icon>
-                                  </v-btn>
-                                </template>
-                                <span>remove</span>
-                              </v-tooltip>
-                              <v-tooltip bottom>
-                                <template #activator="{ on, attrs }">
-                                  <v-btn
-                                    v-show="
-                                      i ==
-                                      editedItem.powerTransformer.length - 1
-                                    "
-                                    color="primary"
-                                    small
-                                    icon
-                                    v-bind="attrs"
-                                    @click="
-                                      add(input, editedItem.powerTransformer)
-                                    "
-                                    v-on="on"
-                                  >
-                                    <v-icon>mdi-plus</v-icon>
-                                  </v-btn>
-                                </template>
-                                <span>add</span>
-                              </v-tooltip>
-                            </v-col>
-
-                            <v-col cols="12">
-                              <div class="d-flex">
-                                <v-spacer />
-                              </div>
-                            </v-col>
-                          </v-row>
-                        </v-expansion-panel-content>
-                      </v-expansion-panel>
-                    </v-expansion-panels>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-spacer />
-                <v-btn small tile @click.native="close"> Cancel </v-btn>
-                <v-btn
-                  small
-                  tile
-                  color="primary"
-                  :loading="loading"
-                  @click="save"
-                >
-                  {{ buttonText }}
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+          <!-- Station Dialog -->
+          <station-dialog
+            :item="{ item }"
+            :dialog-prop.sync="dialog"
+            :edited-item-prop.sync="editedItem"
+            :edited-index-prop.sync="editedIndex"
+          />
+          <!-- Station Dialog -->
         </v-toolbar>
       </template>
       <template #[`item.actions`]="{ item }">
@@ -196,7 +32,7 @@
 </template>
 <script>
 import { Confirm, Notify, Report } from 'notiflix'
-import { mapActions, mapGetters, mapState } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 import rules from '~/mixins/rules'
 
 export default {
@@ -204,10 +40,9 @@ export default {
   mixins: [rules],
   data() {
     return {
-      panel: [0],
       title: 'Station',
-      dialog: false,
       loading: false,
+      dialog: false,
       editedIndex: -1,
       editedItem: {
         name: '',
@@ -253,79 +88,15 @@ export default {
 
   computed: {
     ...mapGetters('station', ['stationTableData']),
-    ...mapState('area-office', {
-      areaOffices: 'areaOffices',
-      area_office_error: 'error',
-      area_office_error_message: 'error_message',
-    }),
-    ...mapState('station', {
-      station_error: 'error',
-      station_error_message: 'error_message',
-    }),
-    ...mapGetters('area-office', ['areaOfficeNames']),
-    formTitle() {
-      return this.editedIndex === -1 ? 'New Station' : 'Edit Station'
-    },
-    buttonText() {
-      return this.editedIndex === -1 ? 'Save' : 'Update'
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close()
-    },
   },
 
   methods: {
-    ...mapActions('station', ['addStation', 'updateStation', 'deleteStation']),
-
-    add(object, fieldType) {
-      const newObject = {}
-
-      // eslint-disable-next-line no-unused-vars
-      for (const [key, value] of Object.entries(object)) {
-        newObject[key] = ''
-      }
-      fieldType.push(newObject)
-    },
-    remove(index, fieldType) {
-      fieldType.splice(index, 1)
-    },
-
-    async save() {
-      this.loading = true
-      if (this.editedIndex > -1) {
-        await this.updateStation(this.editedItem)
-      } else {
-        await this.addStation(this.editedItem)
-      }
-      this.loading = false
-      // TODO: revisit this
-      if (this.station_error) {
-        return
-      }
-      this.close()
-      Notify.success(
-        `${this.editedItem.name} has been ${
-          this.editedIndex > -1 ? 'updated' : 'added'
-        }`
-      )
-    },
+    ...mapActions('station', ['deleteStation']),
 
     editItem(item) {
       this.editedIndex = this.stationTableData.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
-    },
-
-    close() {
-      this.dialog = false
-      this.$store.commit('station/SET_ERROR')
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
     },
 
     deleteItem(item) {
@@ -351,9 +122,7 @@ export default {
             Report.failure('Error', response.data.message, 'OK')
           }
         },
-        () => {
-          this.close()
-        }
+        () => {}
       )
     },
   },

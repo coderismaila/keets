589544e00d706e14518147b4e2c3a516ed608765
Ismaila Hassan <code.ismailah28@gmail.com>
2022-02-14 20:02:14 +0100

    fix: center signin card and set a fix width

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,58 +1,63 @@
 <template>
   <v-container class="mt-8">
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card class="pa-8">
-          <v-card-title class="headline justify-center">Sign In </v-card-title>
-          <v-alert v-if="error" type="error" dismissible dense>
-            {{ error_message }}
-          </v-alert>
-          <v-form v-model="valid" lazy-validation class="mt-8">
-            <v-text-field
-              v-model="formData.username"
-              outlined
-              placeholder="Username, Staff ID or Email"
-              prepend-inner-icon="mdi-account"
-              :rules="[rules.required]"
-            />
-            <v-text-field
-              v-model="formData.password"
-              outlined
-              placeholder="Password"
-              :type="hidePassword ? 'password' : 'text'"
-              prepend-inner-icon="mdi-lock"
-              :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required]"
-              @click:append="() => (hidePassword = !hidePassword)"
-            />
-            <nuxt-link to="/auth/forgot-password" class="block"
-              >Forgot password?</nuxt-link
-            >
-            <v-btn
-              class="mt-8"
-              color="primary"
-              block
-              large
-              :loading="loading"
-              type="submit"
-              :disabled="!valid"
-              @click.prevent="handleSubmit"
-              >sign in</v-btn
-            >
-            <p class="mt-4 text-center">
-              Don't have an account?
-              <nuxt-link to="/auth/signup">Sign Up </nuxt-link>
-            </p>
-          </v-form>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-card width="450px" shaped class="pa-12 mx-auto">
+      <nuxt-link to="/">
+        <v-img
+          src="/images/logo.png"
+          height="50px"
+          width="50px"
+          class="d-block mx-auto"
+        ></v-img>
+      </nuxt-link>
+      <v-card-title class="mt-3 px-0 justify-center headline">
+        Welcome Back, Sign In
+      </v-card-title>
+      <v-alert v-if="error" type="error" dismissible dense>
+        {{ error_message }}
+      </v-alert>
+      <v-form v-model="valid" lazy-validation class="mt-6">
+        <v-text-field
+          v-model="formData.username"
+          outlined
+          placeholder="Username, Staff ID or Email"
+          prepend-inner-icon="mdi-account"
+          :rules="[required]"
+        />
+        <v-text-field
+          v-model="formData.password"
+          outlined
+          placeholder="Password"
+          :type="hidePassword ? 'password' : 'text'"
+          prepend-inner-icon="mdi-lock"
+          :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[required]"
+          @click:append="() => (hidePassword = !hidePassword)"
+        />
+        <v-btn
+          class="mt-4"
+          color="primary"
+          block
+          x-large
+          :loading="loading"
+          type="submit"
+          :disabled="!valid"
+          @click.prevent="handleSubmit"
+          >sign in</v-btn
+        >
+        <p class="mt-4 text-center">
+          Don't have an account?
+          <nuxt-link to="/auth/signup">Sign Up </nuxt-link>
+        </p>
+      </v-form>
+    </v-card>
   </v-container>
 </template>
 <script>
+import rules from '~/mixins/rules'
 export default {
   name: 'SigninPage',
   auth: false,
+  mixins: [rules],
   layout: 'auth',
   data() {
     return {

1c91fe76e556a9206a903ce35d0dafaa882eec41
Ismaila Hassan <code.ismailah28@gmail.com>
2022-02-15 03:37:18 +0100

    refactor: add more field to signup form

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="mt-8">
-    <v-card shaped width="450px" class="pa-12 mx-auto">
+    <v-card shaped width="450px" class="pa-4 mx-auto">
       <nuxt-link to="/">
         <v-img
           src="/images/logo.png"
@@ -12,57 +12,193 @@
       <v-card-title class="headline justify-center"
         >Create an Account</v-card-title
       >
-      <v-alert v-if="error" type="error" dismissible>
-        {{ error_message }}
-      </v-alert>
-      <v-form ref="form" v-model="valid" lazy-validation class="mt-8">
-        <v-text-field
-          v-model="formData.username"
-          outlined
-          placeholder="Username"
-          prepend-inner-icon="mdi-account"
-          :rules="[required, min4]"
-        />
-        <v-text-field
-          v-model="formData.staffId"
-          outlined
-          placeholder="Staff ID"
-          prepend-inner-icon="mdi-numeric"
-          :rules="[required, min4, max6]"
-        />
-        <v-text-field
-          v-model="formData.email"
-          type="email"
-          outlined
-          placeholder="Email"
-          prepend-inner-icon="mdi-email"
-          :rules="[required, email]"
-        />
-        <v-text-field
-          v-model="formData.password"
-          outlined
-          placeholder="Password"
-          :type="hidePassword ? 'password' : 'text'"
-          prepend-inner-icon="mdi-lock"
-          :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[required, min8]"
-          @click:append="() => (hidePassword = !hidePassword)"
-        />
+
+      <v-card-text>
+        <v-alert v-if="error" type="error" dismissible>
+          {{ error_message }}
+        </v-alert>
+      </v-card-text>
+      <v-window v-model="step">
+        <v-window-item :value="1">
+          <v-card flat>
+            <v-card-title>Account Info</v-card-title>
+            <v-card-text>
+              <v-form ref="accountForm" v-model="accountValid" lazy-validation>
+                <v-text-field
+                  v-model="formData.username"
+                  outlined
+                  label="Username"
+                  prepend-inner-icon="mdi-account"
+                  :rules="[required, min4]"
+                />
+                <v-text-field
+                  v-model="formData.staffId"
+                  outlined
+                  label="Staff ID"
+                  prepend-inner-icon="mdi-numeric"
+                  :rules="[required, min4, max6]"
+                />
+                <v-text-field
+                  v-model="formData.email"
+                  type="email"
+                  outlined
+                  label="Email"
+                  prepend-inner-icon="mdi-email"
+                  :rules="[required, email]"
+                />
+
+                <v-text-field
+                  v-model="formData.password"
+                  outlined
+                  label="Password"
+                  :type="hidePassword ? 'password' : 'text'"
+                  prepend-inner-icon="mdi-lock"
+                  :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[required, min8]"
+                  @click:append="() => (hidePassword = !hidePassword)"
+                />
+                <!-- <v-btn
+              type="submit"
+              color="primary"
+              x-large
+              block
+              :disabled="!valid"
+              :loading="loading"
+              @click.prevent="handleSubmit"
+              >sign up</v-btn
+            > -->
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item :value="2">
+          <v-card flat>
+            <v-card-title>Basic Info</v-card-title>
+            <v-card-text>
+              <v-form ref="infoForm" v-model="infoValid" lazy-validation>
+                <v-text-field
+                  v-model="formData.firstName"
+                  outlined
+                  label="First Name"
+                  prepend-inner-icon="mdi-account"
+                  :rules="[required]"
+                />
+                <v-text-field
+                  v-model="formData.lastName"
+                  outlined
+                  label="Last Name"
+                  prepend-inner-icon="mdi-numeric"
+                  :rules="[required]"
+                />
+                <v-text-field
+                  v-model="formData.middleName"
+                  outlined
+                  label="Middle Name(Optional)"
+                  prepend-inner-icon="mdi-numeric"
+                />
+                <!-- <v-text-field
+                v-model="formData.phoneNumber"
+                type="email"
+                outlined
+                label="Phone Number"
+                prepend-inner-icon="mdi-phone-outline"
+                :rules="[required]"
+              /> -->
+
+                <!-- <date-picker
+                :date-prop.sync="formData.birthDate"
+                :text-field-props="{}"
+                title="Birth Date"
+              /> -->
+
+                <v-radio-group
+                  v-model="formData.gender"
+                  row
+                  label="Gender"
+                  :rules="[required]"
+                >
+                  <v-radio label="Male" value="MALE"></v-radio>
+                  <v-radio label="Female" value="FEMALE"></v-radio>
+                </v-radio-group>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item :value="3">
+          <v-card flat>
+            <v-card-title>Work Info</v-card-title>
+            <v-card-text>
+              <v-form ref="workForm" v-model="workValid" lazy-validation>
+                <v-select
+                  v-model="formData.jobDescriptionId"
+                  :items="$store.state.jobdescription.jobDescriptions"
+                  item-text="name"
+                  item-value="id"
+                  outlined
+                  label="Job Description"
+                  prepend-inner-icon="mdi-email"
+                  :rules="[required]"
+                />
+                <v-select
+                  v-model="formData.designation"
+                  outlined
+                  :items="designations"
+                  item-text="text"
+                  item-value="value"
+                  label="Designation"
+                  prepend-inner-icon="mdi-email"
+                  :rules="[required]"
+                />
+                <v-select
+                  v-model="formData.areaOfficeId"
+                  :items="$store.state.areaoffice.areaOffices"
+                  item-text="name"
+                  item-value="id"
+                  outlined
+                  label="Area Office"
+                  prepend-inner-icon="mdi-email"
+                  :rules="[required]"
+                />
+                <v-select
+                  v-model="formData.stationId"
+                  :items="$store.state.station.stations"
+                  item-text="name"
+                  item-value="id"
+                  outlined
+                  label="Station"
+                  prepend-inner-icon="mdi-email"
+                />
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
+      <v-card-actions>
+        <v-btn :disabled="step === 1" text @click="step--"> Back </v-btn>
+        <v-spacer></v-spacer>
         <v-btn
+          v-if="step === 3"
+          :disabled="!workValid"
           type="submit"
           color="primary"
-          x-large
-          block
-          :disabled="!valid"
           :loading="loading"
           @click.prevent="handleSubmit"
           >sign up</v-btn
         >
-        <p class="mt-4 text-center">
-          Already have an account?
-          <nuxt-link to="/auth/signin">Sign in </nuxt-link>
-        </p>
-      </v-form>
+        <v-btn
+          v-else
+          :disabled="step === 3 || !accountValid || !infoValid"
+          color="primary"
+          depressed
+          @click="nextForm()"
+        >
+          Next
+        </v-btn>
+      </v-card-actions>
+      <v-card-text class="d-flex justify-center align-center">
+        Already have an account?
+        <nuxt-link to="/auth/signin" class="ml-2"> Sign in </nuxt-link>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -76,7 +212,10 @@ export default {
   layout: 'auth',
   data() {
     return {
-      valid: true,
+      step: 1,
+      accountValid: true,
+      infoValid: true,
+      workValid: true,
       hidePassword: true,
       loading: false,
       showPassword: false,
@@ -87,12 +226,37 @@ export default {
         email: '',
         staffId: '',
         password: '',
+        firstName: '',
+        lastName: '',
+        middleName: '',
+        gender: '',
+        jobDescriptionId: '',
+        birthDate: this.$dayjs().toISOString(),
+        // phoneNumber: '',
+        areaOfficeId: '',
+        designation: '',
+        stationId: '',
       },
+      designations: [
+        { text: 'Chief Technical Office', value: 'CHIEF_TECHNICAL_OFFICER' },
+        { text: 'Head of Unit', value: 'HEAD_OF_UNIT' },
+        { text: 'Technical Manager', value: 'TECHNICAL_MANAGER' },
+        { text: 'Team Lead', value: 'TEAM_LEAD' },
+        { text: 'Team Member', value: 'TEAM_MEMBER' },
+      ],
     }
   },
+  async fetch() {
+    await Promise.all([
+      this.$store.dispatch('station/getAllStations'),
+      this.$store.dispatch('areaoffice/getAllAreaOffices'),
+      this.$store.dispatch('jobdescription/getAlljobDescriptions'),
+    ])
+  },
+
   methods: {
     async handleSubmit() {
-      if (!this.$refs.form.validate()) return
+      if (!this.$refs.workForm.validate()) return
 
       this.error = false
       this.error_message = ''
@@ -108,6 +272,20 @@ export default {
         this.error_message = response.data.message
         this.loading = false
       }
+    },
+    validateForm() {
+      if (this.step === 1) {
+        return this.$refs.accountForm.validate()
+      } else if (this.step === 2) {
+        return this.$refs.infoForm.validate()
+      } else if (this.step === 3) {
+        return this.$refs.workForm.validate()
+      }
+    },
+    nextForm() {
+      if (!this.validateForm()) return
+
+      this.step++
     },
   },
 }

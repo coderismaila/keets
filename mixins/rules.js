2a8ba75a532a877451e792d69520270dc3a525f9
Ismaila Hassan <code.ismailah28@gmail.com>
2022-02-16 16:58:15 +0100

    add number validation

--- mixins/rules.js
+++ mixins/rules.js
@@ -3,6 +3,7 @@ export default {
   data() {
     return {
       required: (v) => !!v || 'Required.',
+      isNumber: (v) => typeof v === 'number' || 'must be number',
       min8: (v) => v.length >= 8 || 'Min 8 characters',
       min4: (v) => v.length >= 4 || 'Min 4 characters',
       max6: (v) => v.length <= 6 || 'Max 6 characters',
